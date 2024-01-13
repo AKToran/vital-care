989be7aba7602fd8e3241a7c88aefb6588b79d35
AKToran <aktoran60@gmail.com>
2024-01-13 22:16:31 +0600

    contact us viewsets and routers

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .import models
+from . serializers import ContactUsSerializer
 
-# Create your views here.
+class ContactUsViewset(viewsets.ModelViewSet):
+    queryset = models.ContactUs.objects.all()
+    serializer_class = ContactUsSerializer
