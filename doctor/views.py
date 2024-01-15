bf593cc161be297a540f5c6d3dd32275069e9b75
AKToran <aktoran60@gmail.com>
2024-01-15 23:11:16 +0600

    pagination and filtering

--- doctor/views.py
+++ doctor/views.py
@@ -2,11 +2,21 @@ from rest_framework import viewsets
 from .import models
 from . serializers import DoctorSerializer, SpecializationSerializer, DesignationSerializer, AvailableTimeSerializer, ReviewSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import filters, pagination
+
+class DoctorPagination(pagination.PageNumberPagination):
+    page_size = 1 #Item per page
+    page_size_query_param = page_size
+    max_page_size = 100
+
 
 class DoctorViewset(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.Doctor.objects.all()
     serializer_class = DoctorSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username','user__first_name', 'user__last_name', 'designation__name', 'specialization__name']
+    pagination_class = DoctorPagination
 
 class SpecializationViewset(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -18,10 +28,18 @@ class DesignationViewset(viewsets.ModelViewSet):
     queryset = models.Designation.objects.all()
     serializer_class = DesignationSerializer
 
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        doctor_id = request.query_params.get('doctor_id')
+        if doctor_id:
+            return queryset.filter(doctor=doctor_id)
+        return queryset
+
 class AvailableTimeViewset(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.AvailableTime.objects.all()
     serializer_class = AvailableTimeSerializer
+    filter_backends = [AvailableTimeForSpecificDoctor]
 
 class ReviewViewset(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
