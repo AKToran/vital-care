0a2be74a76569f47de5b90b74d66a9ba03b5dc8d
AKToran <aktoran60@gmail.com>
2024-01-15 22:37:01 +0600

    securing api and sending appointment mail

--- doctor/views.py
+++ doctor/views.py
@@ -1,24 +1,30 @@
 from rest_framework import viewsets
 from .import models
 from . serializers import DoctorSerializer, SpecializationSerializer, DesignationSerializer, AvailableTimeSerializer, ReviewSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class DoctorViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.Doctor.objects.all()
     serializer_class = DoctorSerializer
 
 class SpecializationViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.Specialization.objects.all()
     serializer_class = SpecializationSerializer
 
 class DesignationViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.Designation.objects.all()
     serializer_class = DesignationSerializer
 
 class AvailableTimeViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.AvailableTime.objects.all()
     serializer_class = AvailableTimeSerializer
 
 class ReviewViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.Review.objects.all()
     serializer_class = ReviewSerializer
 
--- service/views.py
+++ service/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .import models
 from . serializers import SeviceSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class ServiceViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = models.Service.objects.all()
     serializer_class = SeviceSerializer
