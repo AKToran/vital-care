38ae54856a2d1ba99464c79dd4eb5e187dd2bca4
AKToran <aktoran60@gmail.com>
2024-01-16 12:06:57 +0600

    vercel

--- vital_care/settings.py
+++ vital_care/settings.py
@@ -19,6 +19,10 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vital_care.settings")
+application = get_wsgi_application()
+app = application
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
